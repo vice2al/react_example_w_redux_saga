bb4f1a180b8a054d1068cc2dcf1d1ef2988fd265
vice2al <vo.almeida@campus.fct.unl.pt>
2020-08-26 11:33:54 +0100

    Firt step of the Redux-Saga tutorial. This required an update to the way the store is created, moving the logic from Redux/store.js to index.js

--- src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import recipesReducer from './recipesSlice'
-
-export default configureStore({
-  reducer: {
-    recipes: recipesReducer
-  }
-})
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+import "regenerator-runtime"
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './Redux/store'
-import App from './App'
+import { createStore, applyMiddleware, combineReducers  } from 'redux'
+import createSagaMiddleware from 'redux-saga';
+import { helloSaga } from './Saga/sagas';
+import App from './App';
+import recipesReducer from './Redux/recipesSlice';
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  combineReducers({
+  recipes: recipesReducer,
+}),
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(helloSaga);
 
 const wrapper = document.getElementById("container");
+
 wrapper ? ReactDOM.render(
 	<Provider store={store}>
 		<App/>
